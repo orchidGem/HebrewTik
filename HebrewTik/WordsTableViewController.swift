e58323499ffc4e33f82a4c26aba59618ac30c276
Laura Evans <lauraevans@Lauras-MacBook-Pro-2.local>
2016-10-23 17:34:27 +0300

    add wordstableviewcontroller

---
+++ HebrewTik/WordsTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WordsTableViewController.swift
+//  HebrewTik
+//
+//  Created by Laura Evans on 10/23/16.
+//  Copyright © 2016 Laura Evans. All rights reserved.
+//
+
+import UIKit
+
+class WordsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
