b19d1204420f52ec1c8c2e209616a5e093c87659
Laura Evans <lauraevans@Lauras-MacBook-Pro-2.local>
2016-10-23 19:00:01 +0300

    add AddWordViewController

---
+++ HebrewTik/AddWordViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddWordViewController.swift
+//  HebrewTik
+//
+//  Created by Laura Evans on 10/23/16.
+//  Copyright © 2016 Laura Evans. All rights reserved.
+//
+
+import UIKit
+
+class AddWordViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func cancel(_ sender: AnyObject) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
