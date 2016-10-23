7701b32fbaf00ec3c9b779651a87c901e54136a1
Laura Evans <lauraevans@Lauras-MacBook-Pro-2.local>
2016-10-23 16:23:57 +0300

    inital commit

---
+++ HebrewTik/category.swift
@@ -0,0 +1,14 @@
+//
+//  category.swift
+//  HebrewTik
+//
+//  Created by Laura Evans on 10/23/16.
+//  Copyright © 2016 Laura Evans. All rights reserved.
+//
+
+import Foundation
+
+enum Category {
+    case verb
+    case noun
+}
---
+++ HebrewTik/word.swift
@@ -0,0 +1,28 @@
+//
+//  word.swift
+//  HebrewTik
+//
+//  Created by Laura Evans on 10/23/16.
+//  Copyright © 2016 Laura Evans. All rights reserved.
+//
+
+import Foundation
+
+class Word {
+    var id: Int!
+    var datedAdded: NSDate!
+    var text: String!
+    var category: Category!
+    var translation: String?
+    var audio: String?
+    var example: String?
+    var notes: String?
+    
+    init(id: Int, text: String, category: Category) {
+        self.id = id
+        self.text = text
+        self.category = category
+    }
+    
+}
+
